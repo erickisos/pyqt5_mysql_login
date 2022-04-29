11c77f0d9eda4cfb8c130e166fa2e13929bfb2f1
Erick G. Islas-Osuna <erickisos653@gmail.com>
2022-04-28 23:57:15 -0500

    add validation for password

--- src/pyqt_crud/models/user.py
+++ src/pyqt_crud/models/user.py
@@ -19,3 +19,11 @@ class User:
         assert len(value) >= 8, 'Username should have at least 8 characters'
         assert len(value) <= 24, 'Username should have maximum 24 characters'
         return value
+
+    @validator('password')
+    def password_size(cls, value: Text):
+        assert len(value) >= 8, 'Password should have at least 8 characters'
+        assert (
+            len(value) <= 24
+        ), 'Password should have maximum of 24 characters'
+        return value
